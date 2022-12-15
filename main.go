769bd8bedd494c8bf7b292df53e64eef9fbcf742
unknown <bilza32611@gmail.com>
2022-12-16 00:53:42 +0500

    struct by ref and by value

--- main.go
+++ main.go
@@ -1,11 +1,41 @@
 package main
 import ("fmt"
-	"github.com/bilza2023/learningGo/util"
+	// "github.com/bilza2023/learningGo/util"
 		)
 
+	type UserData struct {
+		firstName string
+		lastName string
+		age uint
+		isMember bool
+		amountDue float32
+	}
+
 func main(){
-greetings := util.Greetings()
-fmt.Println("Return from util" , greetings)
+	
+fristUser := UserData { "Mike" , "Tyson" , 50 , true , 87.33}
+fmt.Println("fristUser.firstName", fristUser.firstName)
+fmt.Println("fristUser.lastName", fristUser.lastName)
+
+//--this will not change the original struct (firstUser)
+secondUser := createNewUser(fristUser)
+ 
+fmt.Printf("secondUser %v \n",secondUser)
+
+//--this will Change the original struct (firstUser)
+createOrignalUser( &fristUser)
+ 
+fmt.Printf("fristUser changed %v \n",fristUser)
 
 }
 
+func createNewUser (userData UserData) UserData {
+	userData.firstName = "John"
+	userData.amountDue += 100
+	return userData
+}
+
+func createOrignalUser (userData * UserData) {
+	userData.firstName = "Orignal Name changed"
+	userData.amountDue = 10000
+}
